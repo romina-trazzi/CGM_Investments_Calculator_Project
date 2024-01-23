96a50943e695d12ff6ad6ce5fae263b73b593bab
Romina Trazzi <romina.trazzi@posta.it>
2024-01-23 03:57:16 +0100

    Form graphic

--- src/components/investments_form/Investment_Form.jsx
+++ src/components/investments_form/Investment_Form.jsx
@@ -1,27 +1,30 @@
 import './Investment_Form.css'
 function Investment_Form() {
   return (
-    <div className="form_wrapper">
-        <form className="input-group">
-            <div>
+    <form>
+        
+        <div className="input_group">
+            <div id="user-input">
                 <label>INITIAL INVESTMENT</label>
                 <input type="number" id="initial-investment" placeholder="Initial Investment" min="0" />
             </div>
-            <div>
+            <div id="user-input">
                 <label>ANNUAL INVESTMENT</label>
                 <input type="number" id="annual-investment" placeholder="Annual Investment" min="0" />
             </div>
-            <div>
+        </div>
+
+        <div className="input_group">
+            <div id="user-input">
                 <label>EXPECTED RETURN</label>
                 <input type="number" id="expected-return" placeholder="Expected Return" min="0"/>
             </div>
-            <div>
+            <div id="user-input">
                 <label>DURATION</label>
                 <input type="number" id="duration" placeholder="Duration" min="0" max="20"/>                     
             </div>
-        </form>    
-    
-    </div>
+        </div>
+    </form>    
   )
 }
 
